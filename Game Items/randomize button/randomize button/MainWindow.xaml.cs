9d822db8d74cec84d589834b2d1cac553341a60a
SandyCarnevale <ccarnevale_6012@email.ric.edu>
2015-03-26 20:57:30 -0400

    randomize edited

--- Game Items/randomize button/randomize button/MainWindow.xaml.cs
+++ Game Items/randomize button/randomize button/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
+
+
 namespace randomize_button
 {
     /// <summary>
@@ -27,145 +29,59 @@ namespace randomize_button
 
             
         }
-
+        /// <the Boolean Gender() creates a random number generator that determines if the  heros are male or female. it puts them into the array
         public Boolean Gender()
         {
             Random random = new Random();
             bool[] gender;
             gender = new bool[5];
-            int[] heros;
-            heros = new int[5];
             bool male;
 
             for (int i = 0; i < 5; i++)
             {
-                if (i == 0)
-                {
+               
                     int num;
                     num = random.Next(1, 3);
                     if (num == 1)
                     {
                         male = true;
-                        ///<gender[0] = male;
-                        ///<System.Console.WriteLine(gender[0]);
-                        ///<return gender[0];
-                        return male;
+                        gender[i] = male;
+                      ///<here is where I am having issues with the picture. I tried to save it to resources like a few pages suggested.
+                      ///it saved it to a new folder, not the resources section I need and i cant get it to go to the right resources.
+                        Hero1.Background = Properties.Resources.deadladybug2.jpg;
+                        
+                        return gender[i];
                     }
                     else
                     {
                         male = false;
-                        ///<gender[0] = male;
-                        ///<System.Console.WriteLine(gender[0]);
-                        ///<return gender[0];
-                        return male;
+                        gender[i] = male;
+                        ///<same issue here as the one up above
+                        Hero1.Background =  Properties.Resources.frog.jpg;
+                       
+                        return gender[i];
                     }
-                }
-                if (i == 1)
-                {
-                    int num;
-                    num = random.Next(1, 3);
-                    if (num == 1)
-                    {
-                        male = true;
-                        ///<gender[1] = male;
-                        ///<System.Console.WriteLine(gender[1]);
-                        ///<return gender[1];
-                        return male;
-                    }
-                    else
-                    {
-                        male = false;
-                        ///<gender[1] = male;
-                        ///<System.Console.WriteLine(gender[1]);
-                        ///<return gender[1];
-                        return male;
-                    }
-                }
-                if (i == 2)
-                {
-                    int num;
-                    num = random.Next(1, 3);
-                    if (num == 1)
-                    {
-                        male = true;
-                        ///<gender[2] = male;
-                        ///<System.Console.WriteLine(gender[2]);
-                        ///<return gender[2];
-                        return male;
-                    }
-                    else
-                    {
-                        male = false;
-                        ///<gender[2] = male;
-                        ///<System.Console.WriteLine(gender[2]);
-                        ///<return gender[2];
-                        return male;
-                    }
-                }
-                if (i == 3)
-                {
-                    int num;
-                    num = random.Next(1, 3);
-                    if (num == 1)
-                    {
-                        male = true;
-                        ///<gender[3] = male;
-                        ///<System.Console.WriteLine(gender[3]);
-                        ///<return gender[3];
-                        return male;
-                    }
-                    else
-                    {
-                        male = false;
-                        ///<gender[3] = male;
-                        ///<System.Console.WriteLine(gender[3]);
-                        ///<return gender[3];
-                        return male;
-                    }
-                }
-                if (i == 4)
-                {
-                    int num;
-                    num = random.Next(1, 3);
-                    if (num == 1)
-                    {
-                        male = true;
-                        ///<gender[4] = male;
-                        ///<System.Console.WriteLine(gender[4]);
-                        ///<return gender[4];
-                        return male;
-                    }
-                    else
-                    {
-                        male = false;
-                        ///<gender[4] = male;
-                        ///,System.Console.WriteLine(gender[4]);
-                        ///<return gender[4];
-                        return male;
-                    }
-                }
-            }
+                }    
             return false;
         }
 
-        public String Thing()
-        {
-            bool middle;
-            bool[] sex;
-            sex = new bool[5];
-            for (int m = 0; m < 5; m++)
-            {
-
-                middle = this.Gender();
-                sex[m] = middle;
-                System.Console.WriteLine(middle);
-            }
-            System.Console.WriteLine();
-        }
-
+        ///<this is the start of the randomize button. when you click it, it will call the gender one.
+        /// once it calls the gender it will call the hero one. i havent created the hero one yet.
         private void GenRan_Click(object sender, RoutedEventArgs e)
         {
-            System.Console.WriteLine(this.Thing());
+            this.Gender();
+        }
+
+        /// <this is what will happen if you dont want to randomize the heros.
+        /// it is supposed to make the buttons and text boxes below the hero visible.
+        /// once they are visible i want to make them clickable.
+        /// from there the person will be able to do their choices.
+        private void Hero1_Click(object sender, RoutedEventArgs e)
+        {
+            Hero_Name.Visible = true;
+            Gender_Choices.Visible = true;
+            Female.Visible = true;
+            Male.Visible = true;
         }
     }
 }
