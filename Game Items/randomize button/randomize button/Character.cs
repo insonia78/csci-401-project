d714cf896e3b6b5d50c3385fef142e25a55852c9
insonia78 <tomsa78@hotmail.it>
2015-03-31 15:07:17 -0700

    update randomize button

---
+++ Game Items/randomize button/randomize button/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomize_button
+{
+     
+    class Character
+    {
+        bool gender;
+        public Character()
+        {
+        }
+    public bool Gender
+    {
+          get{return gender;}
+           set{gender = value;} 
+    }
+    }
+}
--- Game Items/randomize button/randomize button/MainWindow.xaml.cs
+++ Game Items/randomize button/randomize button/MainWindow.xaml.cs
@@ -23,6 +23,10 @@ namespace randomize_button
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[] heroType = new int[5];
+            String[] name = new String[5];
+            bool[] gender = new bool[5];
+          
         public MainWindow()
         {
             InitializeComponent();
@@ -30,39 +34,43 @@ namespace randomize_button
             
         }
         /// <the Boolean Gender() creates a random number generator that determines if the  heros are male or female. it puts them into the array
-        public Boolean Gender()
+        public void Gender()
         {
             Random random = new Random();
-            bool[] gender;
-            gender = new bool[5];
-            bool male;
 
+            
+            bool male;
+            int num ;
             for (int i = 0; i < 5; i++)
             {
+                num = random.Next(1, 6);
+                heroType[i] = num;
                
-                    int num;
+                    
                     num = random.Next(1, 3);
                     if (num == 1)
                     {
-                        male = true;
-                        gender[i] = male;
+                        
+                        gender[i] = true;
                       ///<here is where I am having issues with the picture. I tried to save it to resources like a few pages suggested.
                       ///it saved it to a new folder, not the resources section I need and i cant get it to go to the right resources.
                         ///<Hero1.Background = Properties.Resources.deadladybug2.jpg;
                         
-                        return gender[i];
+                        
                     }
                     else
                     {
-                        male = false;
-                        gender[i] = male;
+                        
+                        gender[i] = false;
                         ///<same issue here as the one up above
                         ///<Hero1.Background =  Properties.Resources.frog.jpg;
                        
-                        return gender[i];
+                        
                     }
+                    name[i] = "Bob";
+                    name[i] = getName.Text; 
                 }    
-            return false;
+           
         }
 
         ///<this is the start of the randomize button. when you click it, it will call the gender one.
@@ -70,6 +78,36 @@ namespace randomize_button
         private void GenRan_Click(object sender, RoutedEventArgs e)
         {
             this.Gender();
+            placeCharacter();
+        }
+        public  void placeCharacter()
+        {
+            Character[] characters = new Character[5];
+            for (int i = 0; i < 5; i++)
+            {
+                switch(heroType[i])
+                {
+                    case 0 :
+                        {
+                            characters[i] = new Character();
+                            characters[i].Gender = gender[i];
+
+                            break;
+                        }
+                    default:
+                            {
+                                characters[i] = new Character();
+                                characters[i] .Gender = gender[i];
+                                break;
+                            }
+                       
+                }
+                MessageBox.Show("this is gender in index " + i +" "+ gender[i].ToString());
+               MessageBox.Show("this is characeer in index " + i +" "+characters[i].Gender.ToString());
+               MessageBox.Show("this is in index " + i + " " + characters[i].ToString());
+               
+            }
+
         }
 
         /// <this is what will happen if you dont want to randomize the heros.
@@ -88,6 +126,11 @@ namespace randomize_button
         {
             ///<change the image to a female image
             ///set the array spot to female
+        }
+
+        private void getName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
